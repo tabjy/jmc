c0f4e97d3184da6932b554aac1838bc1efbd249e
kxu <kxu@redhat.com>
2019-08-29 11:54:07 -0400

    fix table colors

--- application/org.openjdk.jmc.joverflow.ui/src/main/java/org/openjdk/jmc/joverflow/ui/util/ColorIndexedArcAttributeProvider.java
+++ application/org.openjdk.jmc.joverflow.ui/src/main/java/org/openjdk/jmc/joverflow/ui/util/ColorIndexedArcAttributeProvider.java
@@ -4,19 +4,32 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Display;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ColorIndexedArcAttributeProvider extends BaseArcAttributeProvider {
 	private final Color COLOR_GRAY = Display.getCurrent().getSystemColor(SWT.COLOR_GRAY);
 
 	private int totalWeight = 0;
 	private int minimumAngle = 0;
 
+	private Map<Object, Color> colors = new HashMap<>();
+
 	public ColorIndexedArcAttributeProvider() {
 		super();
 
 		addListener((event) -> {
 			totalWeight = 0;
+			colors.clear();
+
 			for (Object e : event.getElements()) {
 				totalWeight += getWeight(e);
+			}
+
+			Arrays.sort(event.getElements(), (o1, o2) -> getWeight(o2) - getWeight(o1));
+			for (Object e : event.getElements()) {
+				getColor(e);
 			}
 		});
 	}
@@ -27,10 +40,18 @@ public class ColorIndexedArcAttributeProvider extends BaseArcAttributeProvider {
 
 	@Override
 	public Color getColor(Object element) {
-		if ((double) getWeight(element) / (double) totalWeight * 360f < minimumAngle) {
-			return COLOR_GRAY;
+		Color color = colors.get(element);
+		if (color != null) {
+			return color;
 		}
 
-		return super.getColor(element);
+		if ((double) getWeight(element) / (double) totalWeight * 360f < minimumAngle) {
+			color = COLOR_GRAY;
+		} else {
+			color =  super.getColor(element);
+		}
+
+		colors.put(element, color);
+		return color;
 	}
 }
